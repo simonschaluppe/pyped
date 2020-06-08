ef4b3f60cf0eddc89cbaf343eef0c3b1c4992dba
simon.schneider <simon.schneider@technikum-wien.at>
2020-06-08 10:41:28 +0200

    Initial commit

---
+++ read.py
@@ -0,0 +1,29 @@
+
+import openpyxl
+
+wb = openpyxl.load_workbook("data/PlusenergieExcel_Lisa.xlsm", read_only=True)
+rng = wb.defined_names["META_input"]
+dests = rng.destinations
+
+for title, coord in dests:
+    ws = wb[title]
+    cells= ws[coord]
+
+META = {}
+for col in range(len(cells[0])):
+    META[cells[0][col].value] = cells[1][col].value
+
+
+print(META)
+
+class meta():
+    def __init__(self):
+        self.Summe_NGF = excel_read("Summe_NGF")
+
+
+def (WWt, Qt,StrombedarfausBatterie ):
+    meta.Summe_NGF
+    ascojanscljanbsc
+    for
+
+    return ()
