cf6519c217a0581c790a97da3f40d52fa430c797
simon.schneider <simon.schneider@technikum-wien.at>
2020-06-27 00:57:43 +0200

    openpyxl recoered

--- read.py
+++ read.py
@@ -1,20 +1,20 @@
 
-import openpyxl
-
-wb = openpyxl.load_workbook("data/test.xlsx", read_only=True)
-rng = wb.defined_names["META_input"]
-dests = rng.destinations
-
-for title, coord in dests:
-    ws = wb[title]
-    cells= ws[coord]
-
-META = {}
-for col in range(len(cells[0])):
-    META[cells[0][col].value] = cells[1][col].value
-
-
-print(META)
+# import openpyxl
+#
+# wb = openpyxl.load_workbook("data/test.xlsx", read_only=True)
+# rng = wb.defined_names["META_input"]
+# dests = rng.destinations
+#
+# for title, coord in dests:
+#     ws = wb[title]
+#     cells= ws[coord]
+#
+# META = {}
+# for col in range(len(cells[0])):
+#     META[cells[0][col].value] = cells[1][col].value
+#
+#
+# print(META)
 
 class Meta():
 
