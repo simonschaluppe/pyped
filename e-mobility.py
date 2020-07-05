32d4c2138375361532277a7dd7b6553705a8e311
simon.schneider <simon.schneider@technikum-wien.at>
2020-07-05 13:57:16 +0200

    adds e mobility prototype

---
+++ e-mobility.py
@@ -0,0 +1,45 @@
+
+from random import random, randint
+
+class Battery:
+    capacity    = 100
+    charge      = round(random(), 4)
+    maxcharge   = randint(10,20)
+
+
+class Ecar:
+    states = ["plugged", "trip"]
+    target = randint(50,100)/100
+
+    def __init__(self, id):
+        self.id = f"testcar #{id}"
+        self.state = self.states[0]
+        self.battery = Battery()
+
+
+    def handle_state(self,hour):
+        if self.state == self.states[0]:
+            self.apply_charge()
+
+        elif self.state == self.states[1]:
+            pass
+
+    def charge_bar(self):
+        length = 30
+        bar = "["+"#"*round(self.battery.charge*length)+" "*round(length-self.battery.charge*length)+"]"
+        start = bar[:round(self.target*length)]
+        end = bar[round(self.target*length)+1:]
+        # print(start, end)
+        return "|".join([start, end])
+
+    def __repr__(self):
+        charge_bar = self.charge_bar()
+        # charge_bar[round(self.target/length)+1] = "|"
+        return f"E-car: {charge_bar} charge: {round(self.battery.charge*100)}% > target: {round(self.target*100)}%"
+
+
+
+if __name__ == "__main__":
+    car1 = Ecar(1)
+    for h in range(72):
+        print(car1)
