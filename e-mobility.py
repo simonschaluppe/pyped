9db186e52e18642749280b4a0ebf0510e490257e
simon.schneider <simon.schneider@technikum-wien.at>
2020-07-06 17:14:18 +0200

    change_bar kinda works

--- e-mobility.py
+++ e-mobility.py
@@ -1,23 +1,34 @@
 
 from random import random, randint
+import functools
 
 class Battery:
     capacity    = 100
     charge_state= round(random(), 4)
     maxcharge   = randint(5,12)
     relcharge   = maxcharge / capacity
+
     def charge(self):
-        self.charge_state = min(1, self.charge_state + self.relcharge)
-        return True
+        final = min(1, self.charge_state + self.relcharge)
+        delta = abs(self.charge_state - final)
+        self.charge_state = final
+        return delta
+
     def discharge(self, rate):
         if rate > self.charge_state:
             raise ValueError(f"discharge rate {rate} > charge_state {__name__.charge_state}.")
         else:
-            self.charge_state = max(0, self.charge_state - rate)
+            final = max(0, self.charge_state - rate)
+            delta = abs(self.charge_state - final)
+            self.charge_state = final
+            return -delta
 
 class Ecar:
-    states = ["plugged", "moving "]
+    states = ["plugged", ">moving"]
     target = randint(50,100)/100
+    charged = 0
+    diverted = 0
+    utilized = 0
 
     def __init__(self, id):
         self.id = f"testcar #{id}"
@@ -34,23 +45,59 @@ class Ecar:
             if random() < 0.2:
                 self.state = self.states[1]
         else:
-            if random() < 0.5:
+            if random() < 0.7:
                 self.state = self.states[0]
 
     def handle_state(self):
         if self.state == self.states[0]:
-            self.charge()
+            if self.battery.charge_state < self.target:
+                self.charge()
+            elif self.battery.charge_state > self.target:
+                self.discharge()
         elif self.state == self.states[1]:
             self.move()
 
+    def change_bar(func):
+        @functools.wraps(func)
+        def wrap(self):
+            length = 20
+            str = " "*18+" "*round(self.battery.charge_state * length)
+            change = func(self)
+            change_len = round(abs(change) * length)
+            if change > 0:
+                char = ">"
+                pm = "+"
+            else:
+                char = "<"
+                pm = ""
+                str = str[:len(str)-change_len]
+            change_str = char * change_len
+            print(str+change_str+ f" {pm}{round(change*100)}%")
+        return wrap
+
+    @change_bar
     def move(self):
         req_energy = randint(10, 20)/100
         if self.battery.charge_state > req_energy:
-            self.battery.discharge(req_energy)
+            diff = self.battery.discharge(req_energy)
+            self.utilized += self.battery.capacity * diff
+            return diff
+        else:
+            diff = self.battery.discharge(self.battery.charge_state)
+            self.utilized += self.battery.capacity * diff
+            return diff
 
+    @change_bar
     def charge(self):
-        if self.battery.charge_state < self.target:
-            self.battery.charge()
+        diff = self.battery.charge()
+        self.charged += self.battery.capacity * diff
+        return diff
+
+    @change_bar
+    def discharge(self):
+        diff = self.battery.discharge(randint(5,15)/100)
+        self.diverted += self.battery.capacity * diff
+        return diff
 
     def charge_bar(self):
         length = 20
@@ -63,7 +110,7 @@ class Ecar:
     def __repr__(self):
         charge_bar = self.charge_bar()
         # charge_bar[round(self.target/length)+1] = "|"
-        return f"E-car [{self.state}]: {charge_bar} charge: {round(self.battery.charge_state * 100)}% > target: {round(self.target * 100)}%"
+        return f"E-car [{self.state}]: {charge_bar}"
 
 
 
