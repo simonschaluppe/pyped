75f01ccd69113de712e5f46b3c1a825ec0d00d1c
simon.schneider <simon.schneider@technikum-wien.at>
2020-07-09 00:20:49 +0200

    Adds Meta.from_xlsx , returning a MEta instance with a dict of meta named ranges as parameters and their values

--- read.py
+++ read.py
@@ -1,30 +1,33 @@
 
-# import openpyxl
-#
-# wb = openpyxl.load_workbook("data/test.xlsx", read_only=True)
-# rng = wb.defined_names["META_input"]
-# dests = rng.destinations
-#
-# for title, coord in dests:
-#     ws = wb[title]
-#     cells= ws[coord]
-#
-# META = {}
-# for col in range(len(cells[0])):
-#     META[cells[0][col].value] = cells[1][col].value
-#
-#
-# print(META)
 
+import openpyxl
 import xlwings as xw
 import pdb
 
-class Meta():
+
+
+class Meta:
 
     def __init__(self, meta_names):
-        self.names = meta_names
+        self.__dict__ = meta_names
 
-import xlwings as xw
+    @classmethod
+    def from_xlsx(cls, path):
+        wb = openpyxl.load_workbook(path, read_only=True, data_only=True)
+        rng = wb.defined_names
+        meta_names_dict = {}
+        for name in wb.defined_names.definedName:
+
+            # print(f"{name.name} @ {name.value}")
+            sheet, coords = name.value.split("!")
+            # print(wb[sheet][coords].value)
+            name = name.name.replace("meta_", "")
+            coords = coords.replace("2", "3")
+            meta_names_dict[name] = wb[sheet][coords].value
+
+        return cls(meta_names_dict)
+        # print(META)
+
 
 def from_xlsx(path):
     """
@@ -36,20 +39,22 @@ def from_xlsx(path):
     # TODO: sollte ein Meta() obejct bauen, das den namen in excel speichert, und aber auch den value EINE ZEILE weiter unten
     wb = xw.Book(path)
     meta = {}
+
     for name in wb.names:
         meta[name.name] = {"range": name.refers_to_range, "value": name.refers_to_range.value}
-    # %%
-    wb.close()
+
     print(meta)
-    print("META loaded from {path}")
+    print(f"META loaded from {path}")
     return meta
     #
 
 print(__name__)
 
 if __name__ == "__main__":
-    meta = from_xlsx("data/META.xlsx")
 
-    meta_cls = Meta(meta)
-    meta["meta_building_floor_area"]["value"]
-    pdb.set_trace()
+    # meta = from_xlsx("data/META.xlsx")
+    # meta_cls = Meta(meta)
+    # meta["meta_building_floor_area"]["value"]
+
+    meta_cls2 = Meta.from_xlsx("data/META.xlsx")
+    # pdb.set_trace()
