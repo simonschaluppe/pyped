9e8bc1383838fc5b1725e10a25627a85c127ddb9
simon.schneider <simon.schneider@technikum-wien.at>
2020-09-11 15:50:09 +0200

    first start on csv profiles loading (#6)

--- pyped/read.py
+++ pyped/read.py
@@ -1,8 +1,10 @@
 
-
 import openpyxl
 import xlwings as xw
 import pdb
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 class Property:
     def __init__(self, attributes):
@@ -31,7 +33,7 @@ class Meta:
     #     # print(META)
 
     @classmethod
-    def from_xlsx(cls,path):
+    def from_xlsx(cls ,path):
         """
         reads excel from path and returns dict of named ranges
         :param path:
@@ -42,6 +44,7 @@ class Meta:
         meta = {}
 
         for name in wb.names:
+            # TODO: Check for first qualifier ("meta_"), or rename class in "Inputs" or smth?
             # name or value?
             real_name = name.name.replace("meta_", "").replace("_name", "").replace("_value", "")
             if name.name.endswith("_name"):
@@ -57,7 +60,39 @@ class Meta:
 
         return cls(meta)
 
+
+class Profile:
+
+    def __init__(self, df, unit):
+        self.df = df
+        self.name = self.df.name
+        self.unit = unit
+        self.dt = 8760 / len(df[:]) # hours
+
+    def plot_OALC(self):
+        """Plots the Ordered Annual Load Curve of the Profile"""
+        self.df.plot()
+
+        fig, ax = plt.subplots(1, 1, figsize=(15, 6))
+        self.df.sort_values(ascending=False) \
+                .reset_index(drop=True)\
+                .plot();
+        plt.show()
+
+
+    @classmethod
+    def read_wind(cls, path="../data/profiles/wind.csv"):
+        df = pd.read_csv(path,
+                         delimiter=";",
+                         index_col="H")
+        # first profile:
+        profile = df[df.columns[0]]
+
+        return cls(profile, "MW")
+
 if __name__ == "__main__":
 
-    meta_cls = Meta.from_xlsx("../data/META_name_value.xlsx")
-    # pdb.set_trace()
+    # meta_cls = Meta.from_xlsx("../data/META_name_value.xlsx")
+
+    wind = Profile.read_wind()
+    wind.df.plot()
