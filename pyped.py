f70988bda0959534d2e621f287d53959368007f9
simonschaluppe <simon.schneider@technikum-wien.at>
2020-12-27 01:16:37 +0100

    QV_dT from Luftwechsel, loading sim_inputs

--- pyped.py
+++ pyped.py
@@ -6,24 +6,31 @@
 
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from pyped.excelLoader import load_inputs_from_PEExcel
 
-
+#def simulate():
 TA  = np.genfromtxt("data/profiles/climate.csv",
     delimiter=";")[1:,1]
 
 QS = np.genfromtxt("data/profiles/QS_test.csv") # W/m²
 
-# QS = 9.95 * np.ones((8760,1))
-QI = 4.76 * np.ones((8760,1))   # W/m²
+# PEExcel "Energiesumme"
+Usage = pd.read_csv("data/profiles/usage_profiles.csv", encoding="cp1252")
+QI = Usage["Qi Sommer W/m²"].to_numpy()
 
-QV_dT = 0.5  #W/K/m2
-QV = np.zeros((8760,1))
+# PEExcel "Sim"
+SI = load_inputs_from_PEExcel("data/PlusenergieExcel_Performance.xlsb")
+
+#QV_dT = 0.5  #W/K/m2
+QV_dT = Usage["Luftwechsel_Anlage_1_h"] * 3 * 0.34 # luftwechsel [1/h] * raumhöhe [m] * cp_luft [Wh/m³K] > [W/m²K]
+QV = np.zeros(8760)
 
 QT_dT = 0.7   # W/K/m²
 cI = 200        # Wh/m²K
 
-QT = np.zeros((8760,1))
+QT = np.zeros(8760)
 
 TI = np.zeros(8760)
 TI[0] = 22.
@@ -31,12 +38,12 @@ TI[0] = 22.
 TI_min = 22.
 TI_max = 25.
 
-Qh = np.zeros((8760,1))
-Qc = np.zeros((8760,1))
+Qh = np.zeros(8760)
+Qc = np.zeros(8760)
 
 for t in range(1, 8760):
     dT =  TA[t-1] - TI[t-1]
-    QV[t] = QV_dT * dT # W/m²
+    QV[t] = QV_dT[t] * dT # W/m²
     QT[t] = QT_dT * dT # W/m²
     Q_sum = (QT[t] + QV[t]) + QS[t] + QI[t]
     TI[t] = TI[t-1] + Q_sum / cI
@@ -63,3 +70,12 @@ plt.plot(Qh)
 plt.plot(Qc)
 plt.legend(["QT","QV","QS","QI", "Qh","Qc"])
 plt.show()
+
+plt.plot(QI[:72])
+plt.plot(QV_dT[:72])
+plt.legend(["QI","QV_dT"])
+plt.show()
+
+
+#if __name__ == "__main__":
+    #simulate()
